bc449ec743b6b229b4b14e9107860305686d9c23
HaydenPE <122505968+HaydenPE@users.noreply.github.com>
2023-01-31 12:21:58 -0500

    Add files via upload
    Edited version of Calorimeter.cpp
    
    1/31/2023

--- calorimeter.cpp
+++ calorimeter.cpp
@@ -1,11 +1,16 @@
 #include <functional>
 #include <string>
 #include <iostream>
-
+#include <chrono>
 template <typename Return, typename ...Args>
 void calorimeter(std::function<Return(Args...)> function_to_test, Args... arguments_for_function_to_test) {
     // This will invoke the function that we are going to test.
+    auto start = std::chrono::high_resolution_clock::now();
     function_to_test(arguments_for_function_to_test...);
+    auto end = std::chrono::high_resolution_clock::now();
+    //auto out = end - start;
+    std::chrono::duration<double> out = end - start;
+    std::cout << out.count();
 }
 
 void time_me() {
@@ -13,8 +18,25 @@ void time_me() {
     return;
 }
 
+void fish(int a) {
+    std::cout << "<>< This is a fish. His favorite number is " << a << ".\n";
+    return;
+}
+
+/*void fish(int &a){
+* std::cout << "<>< This is a fish. His favorite number is " << a << ".\n";
+* a++;
+* return;
+* }
+*/
+
+
 int main() {
-    std::function<void(void)> time_me_fn = time_me;
-    calorimeter(time_me_fn);
+    //std::function<void(void)> time_me_fn = time_me;
+    //calorimeter(time_me_fn);
+    //int num_for_fish{ 4 };
+    std::function<void(int)> fish_fn = fish;
+    //calorimeter(fish_fn, num_for_fish);
+    calorimeter(fish_fn, 3);
     return 0;
 }
